b1b587899bde15d92cf647d871309cf86e6e2d10
Larissa Dias <lmdias@uw.edu>
2026-06-16 12:40:51 -0700

    Validation script updates

--- validation.py
+++ validation.py
@@ -1,16 +1,16 @@
-# %% EXAMPLE
-
-# Basic example first
+# %% Validating against GLODAP and comparing with MATLAB ESPERs too
 
 from PyESPER.lir import lir
 from PyESPER.nn import nn
 from PyESPER.mixed import mixed
 import glodap
+import pandas as pd
+import numpy as np
+from scipy.io import savemat
 
 # Create the data
-data = glodap.world()
-
-L = slice(0, 10000) # Start with the first 10,000 measurements
+data = glodap.world() # version 2.2023
+L = slice(0, 10000) # first 10,000 data points
 
 # Create the dictionary of predictors
 PredictorMeasurements = {
@@ -34,56 +34,74 @@ OutputCoordinates = {
         "depth",
     ]
 }
+# Create the list of estimation dates, accounting for leap years and YYYYMMDD
+dates = pd.to_datetime(data["datetime"][L].values)
 
-# Create the list of estimation dates; change this later to decimal year
-EstDates = data.year[L].values.tolist()
+EstDates = (
+    dates.year
+    + (dates.dayofyear - 1)
+    / np.where(dates.is_leap_year, 366, 365)
+).tolist()
 
 # Define the path
-Path = "" 
+Path = ""  # home directory
+# %% Now try PyESPERs LIR
              
-# First trying 
 EstimatesLIR, CoefficientsLIR, UncertaintiesLIR = lir(
-    ['DIC'], 
+    ['TA'], 
     Path, 
     OutputCoordinates, 
     PredictorMeasurements, 
-    EstDates=EstDates,
-    Equations=[2]
+    EstDates=EstDates
     )
-
-EstimatesNN, UncertaintiesNN = nn(
-    ['phosphate'], 
-    Path, 
-    OutputCoordinates, 
-    PredictorMeasurements, 
-    EstDates=EstDates,
-    Equations=[3]
-    )
-
-#EstimatesMixed, UncertaintiesMixed = mixed(
-#    ['pH'], 
-#    Path,
-#    OutputCoordinates, 
-#    PredictorMeasurements,
-#    EstDates=EstDates,
-#    Equations=[15]
-#    )
 
 # DEBUG, unhash as needed
 print('LIR Estimates are:')
-print(EstimatesLIR['DIC2'][300:310]) 
+print(EstimatesLIR['TA1'][300:310]) 
 print('LIR Coefficient A estimates are:')
-print(CoefficientsLIR['DIC2']['Coef A'][5:10])
-print('NN Estimates are:')
-print(EstimatesNN['phosphate3'][400:410])
-print('NN uncertainties are:')
-print(UncertaintiesNN['phosphate3'][501:503])
+print(CoefficientsLIR['TA1']['Coef A'][5:10])
 print('LIR uncertainties are:')
-print(UncertaintiesLIR['TA3'][308:310])
+print(UncertaintiesLIR['TA1'][308:310])
+# %% NN
 
-# Optional format to pandas and save
-#import pandas as pd
-#(pd.DataFrame.from_dict(data=EstimatesLIR, orient='index')
-#    .to_csv('TA1LIR.csv'))
-#(pd.DataFrame.from_dict(data=EstimatesNN, orient='index')
-#    .to_csv('EstNN.csv'))
+EstimatesNN, UncertaintiesNN = nn(
+    ['TA'], 
+    Path, 
+    OutputCoordinates, 
+    PredictorMeasurements, 
+    EstDates=EstDates
+    )
+
+# DEBUG, unhash as needed
+print('NN Estimates are:')
+print(EstimatesNN['TA10'][300:310]) 
+print('NN uncertainties are:')
+print(UncertaintiesNN['TA10'][308:310])
+# %% Mixed
+
+EstimatesMixed, UncertaintiesMixed = mixed(
+    ['TA'], 
+    Path,
+    OutputCoordinates, 
+    PredictorMeasurements,
+    EstDates=EstDates
+    )
+
+# DEBUG, unhash as needed
+print('Mixed Estimates are:')
+print(EstimatesMixed['TA3'][300:310]) 
+print('Mixed uncertainties are:')
+print(UncertaintiesMixed['TA3'][308:310])
+# %% Save
+
+savemat(
+    "validation_review_sandborn_TA.mat",
+    {
+        "Estimates_LIR": EstimatesLIR,
+        "Estimates_NN": EstimatesNN,
+        "Estimates_Mixed": EstimatesMixed,
+        "Lon": OutputCoordinates["longitude"],
+        "Lat": OutputCoordinates["latitude"],
+        "Depth": OutputCoordinates["depth"],
+    },
+)
